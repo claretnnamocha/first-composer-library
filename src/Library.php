f8f4e4c600de2b3c03c0d17d0cb257305c6a0903
Nnamocha Claret <devclareo@gmail.com>
2019-08-09 17:17:39 +0100

    load content

---
+++ src/Library.php
@@ -0,0 +1,16 @@
+<?php
+namespace Alpha;
+
+/**
+ * A Simple class in php
+ *@author Claret Nnamocha <devclareo@gmail.com>
+ */
+
+class Library
+{
+	
+	function __construct()
+	{
+		print_r('Hello and welcome to your first ever composer library!!');
+	}
+}
